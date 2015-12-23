ebda4684cebf3c8c88bf8ec3b3366fcabcdd8032
wangshubin <wangshubin@eking.mobi>
2015-12-23 13:46:59 +0800

    写ｄｅｍｏ

---
+++ src/test/java/mydemo/Demo.java
@@ -0,0 +1,11 @@
+package mydemo;
+
+/**
+ * Created by wangshubin on 15-12-21.
+ */
+public class Demo {
+    public static void main(String[] args){
+        System.out.println("Hello World!1111");
+
+    }
+}
